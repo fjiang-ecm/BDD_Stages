2b11d09b99fc6a36418502246bca4cebd178a87b
FJ <francois.jiang@centrale-marseille.fr>
2020-02-06 15:35:53 +0100

    Champs uniques MAJ

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,10 +5,14 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity("userName")
+ * @UniqueEntity("email")
  */
+
 class User
 {
     /**
