4b99b3fe9bc347c91b9297b142fbcc804dc033cd
FJ <francois.jiang@centrale-marseille.fr>
2020-02-20 16:29:08 +0100

    Page d'un Stage

--- src/Controller/InternshipController.php
+++ src/Controller/InternshipController.php
@@ -9,12 +9,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class InternshipController extends AbstractController
 {
     /**
-     * @Route("/stages", name="internship")
+     * @Route("/stages", name="internships")
      */
     public function index()
     {
         $repo = $this->getDoctrine()->getRepository(Internship::class);
         $stages = $repo->findAll();
         return $this->render('internship/index.html.twig', ['stages' => $stages]);
+    }
+
+    /**
+     * @Route("/stage/{id}", name="internship")
+     */
+    public function stage($id)
+    {
+        $stage = $this->getDoctrine()->getRepository(Internship::class)->find($id);
+        return $this->render('internship/internship.html.twig', ['stage' => $stage]);
     }
 }
