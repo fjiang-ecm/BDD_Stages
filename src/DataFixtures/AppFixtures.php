e3e9aedd50d5f5e810799791a6228adfb9087930
FJ <francois.jiang@centrale-marseille.fr>
2020-02-26 17:27:47 +0100

    MAJ Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,14 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Entity\User;
 use App\Entity\Category;
 use App\Entity\Internship;
+
+use Faker;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
@@ -20,6 +22,8 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        $faker = Faker\Factory::create('fr_FR');
+
         $a1 = new Category();
         $a1->setName("1A");
         $manager->persist($a1);
@@ -28,40 +32,81 @@ class AppFixtures extends Fixture
         $a2->setName("2A");
         $manager->persist($a2);
 
+        $s8 = new Category();
+        $s8->setName("S8")
+            ->setParent($a2);
+        $manager->persist($s8);
+
+        $s8_l = new Category();
+        $s8_l->setName("S8 Long")
+            ->setParent($a2);
+        $manager->persist($s8_l);
+
+        $s8_e = new Category();
+        $s8_e->setName("S8 Entreprise")
+            ->setParent($a2);
+        $manager->persist($s8_e);
+
+        $cesure = new Category();
+        $cesure->setName("Césure");
+        $manager->persist($cesure);
+
         $a3 = new Category();
-        $a3->setName("3A");
+        $a3->setName("TFR");
         $manager->persist($a3);
 
-        $info = new Category();
-        $info->setName("Info")
-            ->setParent($a1);
-        $manager->persist($info);
+        $user = new User();
+        $hash = $this->encoder->encodePassword($user, 'iiiiiiii');
 
+        $user->setUserName("MCF")
+            ->setFirstName("MC")
+            ->setLastName("F")
+            ->setEmail("mcf@mcf.fr")
+            ->setPassword($hash)
+            ->setRoles(['ROLE_USER']);
+
+        $manager->persist($user);
+        
         for ($i = 1; $i <= 10; $i++){
             $user = new User();
-            $hash = $this->encoder->encodePassword($user, "password$i");
+            $hash = $this->encoder->encodePassword($user, $faker->password);
 
             $user->setUserName("user$i")
-                ->setFirstName("Prénom$i")
-                ->setLastName("Nom$i")
-                ->setEmail("prénom$i.nom$i@centrale-marseille.fr")
+                ->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
                 ->setPassword($hash)
                 ->setRoles(['ROLE_USER']);
 
             $manager->persist($user);
 
             $internship = new Internship();
-            $internship->setTitle("Stage n° $i")
+            $internship->setTitle($faker->jobTitle)
+                ->setAuthor($user)
+                ->setDescription($faker->text)
+                ->setCategory($a1)
+                ->setCity($faker->city)
+                ->setPostalCode($faker->postcode)
+                ->setCountry($faker->country)
+                ->setCompany($faker->company)
+                ->setContact($faker->name)
+                ->setStartedOn($faker->dateTimeThisDecade($max = 'now', $timezone = null))
+                ->setFinishedOn($faker->dateTimeBetween($startDate = $internship->getStartedOn(), $endDate = 'now', $timezone = null));
+
+            $manager->persist($internship);
+
+            $internship = new Internship();
+            $internship->setTitle($faker->jobTitle)
                         ->setAuthor($user)
-                        ->setDescription("Description du stage n° $i")
-                        ->setCategory($info)
-                        ->setCity("Marseille")
-                        ->setPostalCode("13013")
-                        ->setCountry("France")
-                        ->setCompany("GInfo")
-                        ->setContact("Romain Grondin")
-                        ->setStartedOn(new \DateTime())
-                        ->setFinishedOn(new \DateTime());
+                        ->setDescription($faker->text)
+                        ->setCategory($a2)
+                        ->setCity($faker->city)
+                        ->setPostalCode($faker->postcode)
+                        ->setCountry($faker->country)
+                        ->setCompany($faker->company)
+                        ->setContact($faker->name)
+                        ->setStartedOn($faker->dateTimeThisDecade($max = 'now', $timezone = null))
+                        ->setFinishedOn($faker->dateTimeBetween($startDate = $internship->getStartedOn(), $endDate = 'now', $timezone = null));
 
             $manager->persist($internship);
         }
